3e2e7b34da2a8a88439bc8c9b3f00bbf36e54416
xijiayu163 <xijiayu@163.com>
2017-03-09 22:12:15 +0800

    webservice soap

--- NetworkCommunication/webservice/src/main/java/com/yu/webservice/endpoint/Client.java
+++ NetworkCommunication/webservice/src/main/java/com/yu/webservice/endpoint/Client.java
@@ -15,18 +15,18 @@ public class Client {
 	public static void main(String[] args) {
 		//访问网络天气
 //		//创建一个MobileCodeWS工厂  
-//        MobileCodeWS factory = new MobileCodeWS();  
-//        //根据工厂创建一个MobileCodeWSSoap对象  
-//        MobileCodeWSSoap mobileCodeWSSoap = factory.getMobileCodeWSSoap();  
-//        //调用WebService提供的getMobileCodeInfo方法查询手机号码的归属地  
-//        String searchResult = mobileCodeWSSoap.getMobileCodeInfo("18512155752", null);  
-//        System.out.println(searchResult);  
+        MobileCodeWS factory = new MobileCodeWS();  
+        //根据工厂创建一个MobileCodeWSSoap对象  
+        MobileCodeWSSoap mobileCodeWSSoap = factory.getMobileCodeWSSoap();  
+        //调用WebService提供的getMobileCodeInfo方法查询手机号码的归属地  
+        String searchResult = mobileCodeWSSoap.getMobileCodeInfo("18512155752", null);  
+        System.out.println(searchResult);  
         
         //访问自定的
-        PhoneManager pm = new PhoneManager();
-        PhoneService phoneServicePort = pm.getPhoneServicePort();
-        Phone mObileInfo = phoneServicePort.getMObileInfo("fdsfd");
-        System.out.println(mObileInfo.getName());
-        System.out.println(mObileInfo.getOwner());
+//        PhoneManager pm = new PhoneManager();
+//        PhoneService phoneServicePort = pm.getPhoneServicePort();
+//        Phone mObileInfo = phoneServicePort.getMObileInfo("fdsfd");
+//        System.out.println(mObileInfo.getName());
+//        System.out.println(mObileInfo.getOwner());
     }
 }
